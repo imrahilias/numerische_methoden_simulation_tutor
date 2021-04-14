923a3abd19d3c5a89786700269f79c2ed7aba33a
imrahilias <siegel.moritz@gmail.com>
2021-04-14 17:49:13 +0200

    rsync

---
+++ sync.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+hd="/mnt/troika/uni/numerische_methoden_simulation_tutor"
+
+## for testing add "--list-only \"
+rsync -aP \
+      --list-only \
+      --include-from=$hd/sync_groups \
+      --exclude='/*/' \
+      msiegel@server4.physprak.tuwien.ac.at:/home/EDV2/ \
+      $hd/groups/ \
+    |& tee $hd/log/rsync_$(date "+%y%m%d%H%M").log
