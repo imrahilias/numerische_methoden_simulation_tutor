6790f70d6300e1257e879c85d55c17896ef93ba0
imrahilias <siegel.moritz@gmail.com>
2021-06-09 21:23:43 +0200

    saj not quite working

--- exercises/saj/saj.f90
+++ exercises/saj/saj.f90
@@ -13,31 +13,32 @@ program saj
   integer, parameter :: imax=254, kmax=333, nn=4 ! only nearest neighbours: set to 4
   integer, dimension(8), parameter :: inn=[1,-1,0,0,1,1,-1,-1] ! nearest & next nearest neighbour coordinates
   integer, dimension(8), parameter :: knn=[0,0,1,-1,1,-1,1,-1]
-  real(kind=p), parameter :: lambda=0.9_p, ti=10000_p, tf=0.001_p ! temperature settings
+  real(kind=p), parameter :: lambda=0.98_p, ti=100_p, tf=0.01_p ! temperature settings
   real(kind=p), parameter :: weight=1._p ! weighting of posterior vs coupling (<1: less coupling, >1: more coupling)
   real(kind=p), dimension(5), parameter :: mv=[30._p,426._p,602._p,1223._p,167._p] ! [bg,wm,gm,csf,sb] mean values
   real(kind=p), dimension(5), parameter :: sigma=[30._p,59._p,102._p,307._p,69._p] ! [bg,wm,gm,csf,sb] standard deviation
   !! ////////////////////////////////////////////////////////////////////////////
   integer, dimension (imax,kmax) :: mr, cor
-  integer :: i,k,n,c=0, idummy,kdummy,inew,knew, typ, amount(5)=0
+  integer :: i,k,n,xn,c=0, idummy,kdummy,inew,knew, typ, amount(5)=0
   real(kind=p), parameter :: size=imax*kmax
-  real(kind=p), dimension (imax,kmax) :: neighbours=0, posterior=0
-  real(kind=p) :: r, t=ti, flips=0, accept=0, enew, eold, jj(5,5)=1, prior(5)=0, dist, jmean, jvar
+  real(kind=p), dimension (imax,kmax) :: posterior=0
+  real(kind=p) :: r, t=ti, flips=0, accept=0, dist, jmean, jvar, x
+  real(kind=p) :: enew, eold, deltae, jj(5,5)=1, jjnew(5,5)=1, prior(5)=0
 
   !! read mr & cor & count types
-  open(unit=101, file='mr.dat')
-  open(unit=102, file='cor.dat')
+  open(unit=10, file='mr.dat')
+  open(unit=20, file='cor.dat')
   do i = 1,imax
      do k = 1,kmax
-        read(101,'(3i5)') idummy, kdummy, mr(i,k)
-        read(102,'(3i5)') idummy, kdummy, cor(i,k)
+        read(10,'(3i5)') idummy, kdummy, mr(i,k)
+        read(20,'(3i5)') idummy, kdummy, cor(i,k)
         amount(cor(i,k)) = amount(cor(i,k)) + 1
      end do
-     read(101,*)
-     read(102,*)
+     read(10,*)
+     read(20,*)
   end do
-  close(unit=101)
-  close(unit=102)
+  close(unit=10)
+  close(unit=20)
 
   !! calculate prior & print type distribution.
   prior = dble(amount) / size
@@ -47,9 +48,10 @@ program saj
   write(*,*) 'typ, amount, prior:'
   write(*,*) ( i, amount(i), prior(i), new_line('A'), i=1,5 )
 
-  !! calculate posterior
-  gridi : do i = 1, imax
-     gridk : do k = 1, kmax
+  !! calculate posterior.
+  open(unit=30, file='pos.dat')
+  do i = 1, imax
+     do k = 1, kmax
         typ = cor(i,k)
 
         !! calculate posterior of current setting.
@@ -59,12 +61,31 @@ program saj
         !! include prior (occurence probability) based on type prevalence.
         !posterior(i,k) = posterior(i,k) + log(prior(typ))
 
-        !! loop over nearest ( & next nearest) neighbors.
-        neigbour : do n = 1, nn
-           dist = 1
-           inew = i + inn(n)
-           knew = k + knn(n)
+        write(30,*) i, k, posterior(i,k)
+     end do
+     write(30,*)
+  end do
+  close(unit=30)
 
+  !! sweeeeep.
+  open(unit=40, file='flips.dat')
+  cool : do while (t .gt. tf)
+     flips = 0
+     c = c + 1
+     t = t * lambda     
+
+     !! calculate likelihood
+     gridi : do i = 1, imax
+        gridk : do k = 1, kmax
+           eold = 0
+           enew = 0
+           
+           !! pick a random neigbour.
+           call random_number( x )
+           xn = int( 5 * x ) + 1
+           inew = i + inn(xn)
+           knew = k + knn(xn)
+           
            !! check periodic boundary conditions.
            if (inew .le. 0) then
               inew = imax
@@ -77,40 +98,12 @@ program saj
               knew = 1
            end if
 
-           !! define distance of current to center position,
-           !! distance is squared vector norm for more than 8 neighbours.
-           if ( inn(n) .ne. 0 .and. knn(n) .ne. 0 ) dist = 1/real(inew*inew + knew*knew, kind=p) 
+           !! set one coupling constant to -10<x<10.
+           call random_number( x )
+           jjnew( cor(i,k), cor(inew,knew) ) = 10 * x
 
-           !! calculate correlation to nn.
-           if ( cor(inew,knew) .eq. typ ) neighbours(i,k) = neighbours(i,k) + dist
-        end do neigbour
-
-        !! norm & write j.
-        if ( neighbours(i,k) .ne. 0 ) jj(i,k) = posterior(i,k)/( neighbours(i,k) * weight )
-
-        write(103,*) i,k,posterior(i,k),neighbours(i,k),j0(i,k)
-
-     end do gridk
-     write(103,*)
-
-  end do gridi
-  close(unit=103)
-
-  !! sweeeeep.
-  open(unit=20, file='flips.dat')
-  open(unit=103, file='j.dat')
-  cool : do while (t .gt. tf)
-     c = c + 1
-     t = t * lambda     
-
-     !! calculate likelihood
-     gridi : do i = 1, imax
-        gridk : do k = 1, kmax
-           typ = cor(i,k)
-
-           !! loop over nearest ( & next nearest) neighbors.
+           !! loop over all nearest ( & next nearest) neighbors.
            neigbour : do n = 1, nn
-              dist = 1
               inew = i + inn(n)
               knew = k + knn(n)
 
@@ -126,42 +119,96 @@ program saj
                  knew = 1
               end if
 
-              !! define distance of current to center position,
               !! distance is squared vector norm for more than 8 neighbours.
-              if ( inn(n) .ne. 0 .and. knn(n) .ne. 0 ) dist = 1/real(inew*inew + knew*knew, kind=p) 
+              dist = 1/real(inew*inew + knew*knew, kind=p) 
 
-              !! calculate correlation to nn.
-              if ( cor(inew,knew) .eq. typ ) neighbours(i,k) = neighbours(i,k) + dist
+              !! calculate energy to nn of old setting.
+              eold = eold - jj( cor(i,k), cor(inew,knew) ) * dist
+
+              !! calculate energy to nn of new setting.
+              enew = enew - jjnew( cor(i,k), cor(inew,knew) ) * dist
+              
            end do neigbour
 
-           !! norm & write j.
-           if ( neighbours(i,k) .ne. 0 ) jj(i,k) = posterior(i,k)/( neighbours(i,k) * weight )
+           !! change in total energy.
+           deltae = (enew - eold) * weight + posterior(i,k)
+           
+           ! check for floating point overflow & define r.
+           if ( deltae/t .gt. 20._p ) then
+              r = 0._p
+           else if ( deltae/t .lt. 0.001_p ) then
+              r = 1._p
+           else
+              r = exp(-deltae/t)
+           end if
 
-           write(103,*) i,k,posterior(i,k),neighbours(i,k),jj(i,k)
+           ! accept or deny via metropolis algorithm.
+           call random_number( x )
+           if ( x .lt. r ) then
+              jj( cor(i,k), cor(inew,knew) ) = jjnew( cor(i,k), cor(inew,knew) )
+              flips = flips + 1
+           end if
 
         end do gridk
-
-        write(103,*)
-
      end do gridi
+
+     !! print
+     write(*,*) 'run: ',c, 'temperature: ',t ,'flips: ',flips
+     write(40,*) c, t, flips
+     accept = accept + flips
 
   end do cool
 
-  !! print coupling distribution.
-  write(*,*) 'coupling distribution:'
-  write(*,*) 'bg \t wm \t gm \t csf \t sb'
-  write(*,*) ( 'bg', jj(1,i), new_line('A'), i=1,5 )
-  write(*,*) ( 'wm', jj(2,i), new_line('A'), i=1,5 )
-  write(*,*) ( 'gm', jj(3,i), new_line('A'), i=1,5 )
-  write(*,*) ( 'csf',jj(4,i), new_line('A'), i=1,5 )
-  write(*,*) ( 'sb', jj(5,i), new_line('A'), i=1,5 )
+  !! print resulting energy.
+  open(unit=50, file='energy.dat')
+  do i = 1, imax
+     do k = 1, kmax
+        enew = 0
+        
+        !! loop over nearest ( & next nearest) neighbors.
+        do n = 1, nn
+           inew = i + inn(n)
+           knew = k + knn(n)
 
-  !! statistics.
-  jmean = sum(jj)/size
-  jvar = sqrt( sum( (jj - jmean)**2 )/size )
-  write(*,*) 'coupling constant statistics:'
-  write(*,*) 'mean of j:', jmean
-  write(*,*) 'var of j:', jvar
+           !! check periodic boundary conditions.
+           if (inew .le. 0) then
+              inew = imax
+           else if(inew .gt. imax) then
+              inew = 1
+           end if
+           if (knew .le. 0) then
+              knew = kmax
+           else if(knew .gt. kmax) then
+              knew = 1
+           end if
+
+           !! distance is squared vector norm for more than 8 neighbours.
+           dist = 1/real(inew*inew + knew*knew, kind=p)
+           
+           !! calculate energy to nn of new setting.
+           enew = enew - jjnew( cor(i,k), cor(inew,knew) ) * dist
+        end do
+        
+        !! change in energy & total energy.
+        write(50,*) i, k, enew, enew * weight + posterior(i,k)
+     end do
+     write(50,*)
+  end do
+  close(unit=50)
+  
+  !! print coupling distribution.
+  open(unit=60, file='jj.dat')
+  write(*,*) 'coupling distribution:'
+  write(*,*) jj
+  write(60,*) ( ( i, k, jj(i,k), new_line('A'), i=1,5 ), k=1,5 )
+  close(unit=60)
+  
+  ! !! statistics.
+  ! jmean = sum(jj)/size
+  ! jvar = sqrt( sum( (jj - jmean)**2 )/size )
+  ! write(*,*) 'coupling constant statistics:'
+  ! write(*,*) 'mean of j:', jmean
+  ! write(*,*) 'var of j:', jvar
 
 
 end program saj
--- exercises/saj/saj.gp
+++ exercises/saj/saj.gp
@@ -1,115 +1,101 @@
+#--------------------------------------------------------------------------------
 reset
-set pm3d map
-set palette gray
+unset key
+set logscale x
+set xlabel 'kbT / a.u.'
+set ylabel 'flips / pixel / sweep'
 set term png
+set output "flips.png"
+plot 'flips.dat' u 2:($3/254/333) w l
+
+#--------------------------------------------------------------------------------
+reset
+set term png
+#set term png size 25,25
+set pm3d map
+set output "jj.png"
+plot "jj.dat"  u 1:2:3 w image
+
+#--------------------------------------------------------------------------------
+reset
+set term png
+set pm3d map
+set output "energy.png"
+plot "energy.dat"  u 1:(-$2):3 w image
+
+#--------------------------------------------------------------------------------
+reset
+set term png
+set pm3d map
+set output "weighted.png"
+plot "energy.dat"  u 1:(-$2):4 w image
+
+
+#--------------------------------------------------------------------------------
+reset
+set term png
+set pm3d map
+set output "pos.png"
+plot "pos.dat" u 1:(-$2):3 w image
+
+#--------------------------------------------------------------------------------
+reset
+unset key
+unset xtics
+unset ytics
+unset colorbox
+unset xlabel
+unset ylabel
+set autoscale xfix
+set autoscale yfix
+set margins 0,0,0,0
+set size ratio -1
+unset xtics
+unset ytics
+set border 0
+set palette gray
+set term png size 254,333
+set pm3d map
 set output "mr.png"
-unset xlabel
-unset ylabel
-set autoscale xfix
-set autoscale yfix
-set size ratio -1
 plot "mr.dat"  u 1:(-$2):3 w image
 
 #--------------------------------------------------------------------------------
 reset
+unset key
+unset xtics
+unset ytics
+unset colorbox
+unset xlabel
+unset ylabel
+set autoscale xfix
+set autoscale yfix
+set margins 0,0,0,0
+set size ratio -1
+unset xtics
+unset ytics
+set border 0
+set term png size 254,333
 set pm3d map corners2color c1
 set palette defined (1 'white', 2 'blue', 3 'red',4 'yellow' ,5 'pink')
-set term png
 set output "cor.png"
-unset xlabel
-unset ylabel
-set autoscale xfix
-set autoscale yfix
-set size ratio -1
 plot "cor.dat"  u 1:(-$2):3 w image
 
 #--------------------------------------------------------------------------------
 reset
-set pm3d map corners2color c1
-set term png
-set log cb
+unset key
+unset xtics
+unset ytics
+unset colorbox
 unset xlabel
 unset ylabel
 set autoscale xfix
 set autoscale yfix
+set margins 0,0,0,0
 set size ratio -1
-set output "j.png"
-plot "j.dat"  u 1:(-$2):3 w image
-
-#--------------------------------------------------------------------------------
-reset
-unset key
-#set size 0.26,0.5
+unset xtics
+unset ytics
+set border 0
+set term png size 254,333
 set pm3d map
-#set dgrid3d 254,333
-set palette gray
-unset xtics
-unset ytics
-unset colorbox
-#set xrange [1:254]
-#set yrange [1:333]
-set term png size 254,333
-set output "mr_raw.png"
-unset xlabel
-unset ylabel
-set autoscale xfix
-set autoscale yfix
-set margins 0,0,0,0
-set size ratio -1
-unset xtics
-unset ytics
-set border 0
-unset key
-plot "mr.dat"  u 1:(-$2):3 w image
-
-#--------------------------------------------------------------------------------
-reset
-unset key
-#set size 0.26,0.5
-set pm3d map corners2color c1
-#set dgrid3d 254,333
-set palette defined (1 'white', 2 'blue', 3 'red',4 'yellow' ,5 'pink')
-unset xtics
-unset ytics
-unset colorbox
-#set xrange [1:254]
-#set yrange [1:333]
-set term png size 254,333
-set output "cor_raw.png"
-unset xlabel
-unset ylabel
-set autoscale xfix
-set autoscale yfix
-set margins 0,0,0,0
-set size ratio -1
-unset xtics
-unset ytics
-set border 0
-unset key
-plot "cor.dat"  u 1:(-$2):3 w image
-
-#--------------------------------------------------------------------------------
-reset
-unset key
-#set size 0.26,0.5
-set pm3d map corners2color c1
-#set dgrid3d 254,333
-unset xtics
-unset ytics
-unset colorbox
-set log cb
-#set xrange [1:254]
-#set yrange [1:333]
-set term png size 254,333
-unset xlabel
-unset ylabel
-set autoscale xfix
-set autoscale yfix
-set margins 0,0,0,0
-set size ratio -1
-unset xtics
-unset ytics
-set border 0
-unset key
-set output "j_raw.png"
-plot "j.dat"  u 1:(-$2):3 w image
+set output "pos_raw.png"
+plot "pos.dat"  u 1:(-$2):3 w image
